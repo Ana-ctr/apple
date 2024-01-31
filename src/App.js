d8aaa8bc6c3619dcd06558e8f90f461da6d670b4
Anara <anara_n7@mail.ru>
2024-01-31 12:45:52 +0100

    text1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Day from "./day";
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Day2 from "./day2";
 function App() {
   return (
     <div className="App"> <BrowserRouter>
       <Routes>
-        <Route path="/Day" element={<Day />}> </Route></Routes>
+        <Route path="/Day" element={<Day />}> </Route>
+       <Route path="/Day2" element={<Day2 />}> </Route>
+       </Routes>
     </BrowserRouter>
     </div>
 
